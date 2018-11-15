2f99aed0ea446bf6e984282c2fb48ef6af243e59
Daniel Streby <dstreby@2u.com>
2018-11-15 12:50:27 -0500

    Rewrote kube-capacity

--- tmux/status/right.sh
+++ tmux/status/right.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 echo "#[fg=colour39,bg=colour234]\
-#[fg=black,bg=colour39] Ѫ \
-$(kubectl config get-contexts --no-headers | awk '/^\*/ {print $4 " : " $5}') \
+#[fg=black,bg=colour39] » \
+$(kubectl config get-contexts --no-headers | awk '/^\*/ {print $2 " : " $5}') \
 #[fg=colour238,bg=colour39]#[fg=yellow,bg=colour238]\
 $(uptime | awk -F: '{print $NF}') \
 #[fg=colour252,bg=colour238]#[fg=colour235,bg=colour252] \
